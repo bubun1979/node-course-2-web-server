b169b1296166fe7e012e89a4007e4173445fe6a6
soumyajit-cn <soumyajit@capitalnumbers.com>
2017-10-07 12:48:16 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (error) => {
+        if(error){
+            console.log("Unable to append log to the server.log file");
+        }
+    });
+    console.log(log);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintainance.hbs', {
+        pageType: "Maintainance Page",
+        maintainMsg: "Work in progress. Please try after some time"
+    });
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageType: "Home Page",
+        welcomeMsg: "Welcome to my site"
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageType: "About Page"
+    });
+});
+
+app.listen(3000, (err) => {
+    console.log("Server running on port 3000");
+});
